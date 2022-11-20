5ec0adc7bbeed803af97251bb556029d96aa52a2
Carlos Augusto <carlos.aumaca@gmail.com>
2022-11-20 11:37:55 -0300

    Sidenav open/close system added

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,33 +1,52 @@
 import React, { useState, useEffect } from "react";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faGlobe, faVrCardboard, faHandshake, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-import '../styles/SideNav.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faVrCardboard, faHandshake, faMagnifyingGlass, faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import '../styles/SideNav.css';
 
-import metaLogo from '../images/metaLogo.png'
+import metaLogo from '../images/metaLogo.png';
 
 export default function SideNav() {
+    const [sideNavIsOpen, setSideNavIsOpen] = useState(false);
+
+    function openSideNav() {
+        if (sideNavIsOpen) {
+            setSideNavIsOpen(false);
+        } else {
+            setSideNavIsOpen(true);
+        }
+    }
+
     return (
-        <div className="sidenav">
-            <div className="header">
-                <img src={metaLogo} alt="" />
+        <>
+            <div className="sidenav__container">
+                <div className={sideNavIsOpen ? "sidenav open" : "sidenav"}>
+                    <div className="header">
+                        <img src={metaLogo} alt="" />
+                    </div>
+                    <div className="topics">
+                        <ul>
+                            <li>
+                                <FontAwesomeIcon icon={faGlobe} />
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faVrCardboard} />
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faHandshake} />
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={sideNavIsOpen ? "sidenav__button open" : "sidenav__button"} onClick={() => openSideNav()}>
+                    <button>
+                        <FontAwesomeIcon icon={sideNavIsOpen ? faAngleLeft : faAngleRight} />
+                    </button>
+                </div>
             </div>
-            <div className="topics">
-                <ul>
-                    <li>
-                        <FontAwesomeIcon icon={faGlobe} />
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faVrCardboard} />
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faHandshake} />
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </>
     )
 }
